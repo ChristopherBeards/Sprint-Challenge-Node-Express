c460aa0ee8e09f9f4318b28ad23ed0c6148ebce2
Christopher Beards <beardscm@gmail.com>
2018-03-09 11:32:47 -0500

    Compare Route Created
    Test Send Successful

--- src/server.js
+++ src/server.js
@@ -11,6 +11,11 @@ let PORT = 3030;
 
 const STATUS_USER_ERROR = 422;
 
+server.get('/compare', (req, res) => {
+  res.send("Testing...");
+});
+
+
 server.listen(3030, (err) => {
   if (err) {
     console.log(`There was an error starting the server: ${err}`);
