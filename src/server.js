11574db581ed16ebfc45c234c6adfc7bcfd3d90f
Christopher Beards <beardscm@gmail.com>
2018-03-09 13:06:46 -0500

    ASYNCH AWAIT-Difference Posts with a new approach

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors'); //middleware
 const request = require('request'); //middleware
 const server = express();
+const fetch = require('node-fetch');
+
 
 server.use(bodyParser.json());
 server.use(cors());
@@ -11,25 +13,44 @@ let PORT = 3030;
 
 const STATUS_USER_ERROR = 422;
 
-server.get('/compare', (req, res) => {
-  let current = 0;
-  let previous = 0;
 
-  request('https://api.coindesk.com/v1/bpi/currentprice/USD.json', (error, response, body) => {
-    let parseObject = JSON.parse(body);
-    current = parseObject.bpi.USD.rate_float;
-  });
-
-  request('https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday', (error, response, body) => {
-    let parseObject = JSON.parse(body);
-    previous = Object.values(parseObject.bpi); 
-    previous = previous[0];
+async function comparePrices(current){
+    const response = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday')
+    let previous = await response.json();
+    previous = Object.values(previous.bpi)[0];
+    current = current.bpi.USD.rate_float;
     let difference = current - previous;
+    return { difference };
+}
 
-
-    res.send({current, previous, difference});
-  });
+server.get('/compare', (req, res) => {
+  fetch('https://api.coindesk.com/v1/bpi/currentprice/USD.json')
+  .then(res => res.json())
+  .then((current) => { 
+    const promise = comparePrices(current);
+    promise.then(difference => res.send(difference));
+  })
 });
+
+// server.get('/compare', (req, res) => {
+//   let current = 0;
+//   let previous = 0;
+
+//   request('https://api.coindesk.com/v1/bpi/currentprice/USD.json', (error, response, body) => {
+//     let parseObject = JSON.parse(body);
+//     current = parseObject.bpi.USD.rate_float;
+//   });
+
+  // request('https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday', (error, response, body) => {
+  //   let parseObject = JSON.parse(body);
+  //   previous = Object.values(parseObject.bpi); 
+  //   previous = previous[0];
+  //   let difference = current - previous;
+
+
+  //   res.send({current, previous, difference});
+  // });
+// });
 
 server.get('*', (req, res) => {
   res.status(STATUS_USER_ERROR);
