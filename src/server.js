324f3c1b83e56f0af496309aa3032c8759274f76
Christopher Beards <beardscm@gmail.com>
2018-03-09 11:30:30 -0500

    Renamed Main from App to Server.js for Personal Preference
    Implemented needed Instantiations and server.listen

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors'); //middleware
+const request = require('request'); //middleware
+const server = express();
+
+server.use(bodyParser.json());
+server.use(cors());
+
+let PORT = 3030;
+
+const STATUS_USER_ERROR = 422;
+
+server.listen(3030, (err) => {
+  if (err) {
+    console.log(`There was an error starting the server: ${err}`);
+  }
+  else {
+    console.log(`Server is listening on port ${PORT}`)
+  }
+});
